fe5ea430ec646bd40c44f7d1ff6789beaed98d69
BenOtman-wq <othmanbenyahy@gmail.com>
2025-05-02 13:22:58 +0100

    changer data base

--- Y/connection.php
+++ Y/connection.php
@@ -1,4 +1,19 @@
+
 <?php
-$dba = new PDO('mysql:host=localhost;dbname=schooldba', "root","");
-$dba->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$host = 'localhost';
+$dbname = 'schooldba';
+$username = 'root';
+$password = '';
+
+
+try {
+    $dba = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $dba->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion reussie!";
+} catch (PDOException $e) {
+    echo "erreur..." . $e->getMessage();
+}
+
 ?>
+
