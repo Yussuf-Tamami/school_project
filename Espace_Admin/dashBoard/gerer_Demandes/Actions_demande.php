72b70170c2fcaa7fb246607f6d48202c23add27a
BenOtman-wq <othmanbenyahy@gmail.com>
2025-05-03 23:03:33 +0100

    data_base_paht_fixed

--- Espace_Admin/dashBoard/gerer_Demandes/Actions_demande.php
+++ Espace_Admin/dashBoard/gerer_Demandes/Actions_demande.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../../connection.php';
+require_once '../../../dataBase/connection.php';
 
 // Vérifier l'authentification admin
 if (!isset($_SESSION['admin_name'])) {
